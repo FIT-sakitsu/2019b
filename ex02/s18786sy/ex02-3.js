557bdeb88c053d1d13c7eb420db9dd56d6bb45ba
矢野颯大 <main@Sotas-MacBookPro.local>
2019-10-01 11:55:55 +0900

    ex02を作成

---
+++ ex02/s18786sy/ex02-3.js
@@ -0,0 +1,26 @@
+var x = 0
+
+function to0() {
+  x = 0
+  alert(x)
+}
+
+function plus1() {
+  x += 1
+  alert(x)
+}
+
+function minus1() {
+  x -= 1
+  alert(x)
+}
+
+function multipleby2() {
+  x *= 2
+  alert(x)
+}
+
+function decidedby2() {
+  x /= 2
+  alert(x)
+}
