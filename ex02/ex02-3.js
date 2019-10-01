3471f40549d38323ce7a0c04908e7ab7c703c5fb
Kanako Sakakibara <s19355ks@zmac137.sfc.keio.ac.jp>
2019-10-01 11:25:26 +0900

    ex02を作成

---
+++ ex02/ex02-3.js
@@ -0,0 +1,27 @@
+var current = 0
+
+function syokyo() {
+  current = 0
+  alert(currrent)
+}
+
+function tasu() {
+  current += 1
+  alert(currrent)
+}
+
+function hiku() {
+  current -= 1
+  alert(currrent)
+}
+
+
+function nibai() {
+  current *= 2
+  alert(currrent)
+}
+
+function hanbunko() {
+  current /= 2
+  alert(currrent)
+}
