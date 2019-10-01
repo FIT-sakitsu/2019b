0dd6b2976ca1e8fc464de6fd454f06d65850fd02
Mao Yamamura <s19850my@zmac146.sfc.keio.ac.jp>
2019-10-01 11:39:11 +0900

    ex02を作成

---
+++ ex02/s19850my/ex02-3.js
@@ -0,0 +1,26 @@
+var current = 0
+
+function reset() {
+  current = 0
+  alert(current)
+}
+
+function plus() {
+  current += 1
+  alert(current)
+}
+
+function minus() {
+  current -= 1
+  alert(current)
+}
+
+function double() {
+  current *= 2
+  alert(current)
+}
+
+function half() {
+  current /= 2
+  alert(current)
+}
