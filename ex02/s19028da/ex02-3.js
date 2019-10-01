6d55f646fc900a434921b46d8ada3b6e1ea967c2
Daiya Arai <s19028da@zmac124.sfc.keio.ac.jp>
2019-10-01 11:51:07 +0900

    ex02を作成

---
+++ ex02/s19028da/ex02-3.js
@@ -0,0 +1,26 @@
+var number = 0
+
+function zero() {
+  number = 0
+  alert(number)
+}
+
+function plusone() {
+  number += 1
+  alert(number)
+}
+
+function minusone() {
+  number -= 1
+  alert(number)
+}
+
+function multiplytwo() {
+  number *= 2
+  alert(number)
+}
+
+function dividedbytwo() {
+  number /= 2
+  alert(number)
+}
