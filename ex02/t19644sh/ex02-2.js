750cd80c1632ae9b64206ca1356a2f37f5bf0524
Sho Hirota <t19644sh@zmac133.sfc.keio.ac.jp>
2019-10-01 11:34:18 +0900

    ex02を作成

---
+++ ex02/t19644sh/ex02-2.js
@@ -0,0 +1,3 @@
+function showMessage() {
+  alert('こんにちは')
+}
---
+++ ex02/t19644sh/ex02-3.js
@@ -0,0 +1,26 @@
+var current = 0
+
+function zero() {
+  current = 0
+  alert(current)
+}
+
+function tasu1() {
+  current += 1
+  alert(current)
+}
+
+function hiku1() {
+  current -= 1
+  alert(current)
+}
+
+function bai() {
+  current *= 2
+  alert(current)
+}
+
+function waru2() {
+  current /= 2
+  alert(current)
+}
