ba757330a756812d0cabe4ffe7043edac6ca87eb
Naoki Hoshino <t19686nh@zmac125.sfc.keio.ac.jp>
2019-10-01 11:50:03 +0900

    ex02を作成

---
+++ ex02/t19686nh/ex02-3.js
@@ -0,0 +1,22 @@
+var number = 0
+
+function zero() {
+  number = 0
+  alert(number)
+}
+function plusone() {
+  number += 1
+  alert(number)
+}
+function minusone() {
+  number -= 1
+  alert(number)
+}
+function twice() {
+  number *= 2
+  alert(number)
+}
+function onehalf() {
+  number /= 2
+  alert(number)
+}
