8e691b6e15b2cecbe4cbdbcc96930b6031c45d5c
Sho Hirota <t19644sh@zmac134.sfc.keio.ac.jp>
2019-10-08 12:27:44 +0900

    ex03を作成

---
+++ ex03/t19644sh/ex03-2.js
@@ -0,0 +1,13 @@
+function fortune() {
+  var num = Math.random()
+
+  if (num < 0.1) {
+    alert('凶')
+  } else if (0.1 <= num && num < 0.33) {
+    alert('小吉')
+  } else if (0.33 <= num && num < 0.66) {
+    alert('中吉')
+  } else if (0.66 < num) {
+    alert('大吉')
+  }
+}
