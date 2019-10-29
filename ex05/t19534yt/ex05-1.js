0158d85bf1523476cfd0993b63b738b867ec0267
Yumiko Toyofuku <t19534yt@zmac132.sfc.keio.ac.jp>
2019-10-29 12:21:21 +0900

    ex05を作成

---
+++ ex05/t19534yt/ex05-1.js
@@ -0,0 +1,9 @@
+var names = []
+
+for (i = 0; i < 6; i++) {
+  var name = prompt('なまえを入力してください。')
+  names.push(name)
+}
+
+alert(name[0] + ',' + names[1] + ',' + names[2])
+alert(name[3] + ',' + names[4] + ',' + names[5])
