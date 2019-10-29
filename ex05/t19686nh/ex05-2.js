d0f6fbda4369b165735b2bf8568505a181f86660
Naoki Hoshino <t19686nh@zmac125.sfc.keio.ac.jp>
2019-10-29 12:39:20 +0900

    第5回課題

---
+++ ex05/t19686nh/ex05-2.js
@@ -0,0 +1,13 @@
+var string = []
+string.push(prompt('文字'))
+string.push(prompt('文字'))
+string.push(prompt('文字'))
+string.push(prompt('文字'))
+string.push(prompt('文字'))
+
+string.sort()
+alert(string[0])
+alert(string[1])
+alert(string[2])
+alert(string[3])
+alert(string[4])
