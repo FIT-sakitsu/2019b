4aac4893525abfae2d53588f26b624f464e12be9
Jaeeun Hwang <t19648jh@zmac138.sfc.keio.ac.jp>
2019-11-05 12:35:12 +0900

    第6回課題

---
+++ ex06/t19648jh/ex06-03.js
@@ -0,0 +1,29 @@
+function changetag1() {
+  var tag1 = document.getElementById('tab1')
+  var tag2 = document.getElementById('tab2')
+  var tag3 = document.getElementById('tab3')
+
+  tab1.style.display = 'block'
+  tab2.style.display = 'none'
+  tab3.style.display = 'none'
+}
+
+function changetag2() {
+  var tag1 = document.getElementById('tab1')
+  var tag2 = document.getElementById('tab2')
+  var tag3 = document.getElementById('tab3')
+
+  tab1.style.display = 'none'
+  tab2.style.display = 'block'
+  tab3.style.display = 'none'
+}
+
+function changetag3() {
+  var tag1 = document.getElementById('tab1')
+  var tag2 = document.getElementById('tab2')
+  var tag3 = document.getElementById('tab3')
+
+  tab1.style.display = 'none'
+  tab2.style.display = 'none'
+  tab3.style.display = 'block'
+}
