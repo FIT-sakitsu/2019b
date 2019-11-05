0545526f7e04e4afdb3997c721052bfed303b561
Saki Okada <s18182so@zmac156.sfc.keio.ac.jp>
2019-11-05 11:31:50 +0900

    ex01を作成

---
+++ ex06/s18182so/ex06-2.js
@@ -0,0 +1,17 @@
+function changeCCC() {
+  var color = document.getElementById("color").value;
+
+  var elements = document.getElementsByClassName("ccc");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor = color;
+  }
+}
+
+function changeAAABBB() {
+  var color = document.getElementById("color").value;
+
+  var elements = document.getElementsByClassName("aaa bbb");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor = color;
+  }
+}
