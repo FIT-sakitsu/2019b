932aa221841af6ba43e5e4e342e5349306929c65
Itsuki Sakitsu <sakitsu@zmac120.sfc.keio.ac.jp>
2019-11-05 10:20:03 +0900

    ex06-0を解いた

---
+++ ex06/sakitsu/ex06-0.js
@@ -0,0 +1,14 @@
+function changeCSS() {
+  // ボタンをクリックしたらテーブルのデザインを変える
+  var table = document.getElementById('table')
+  var currentClassName = table.className
+  switch (currentClassName) {
+    case 'table':
+      currentClassName = 'table table-dark'
+      break
+    case 'table table-dark':
+      currentClassName = 'table'
+      break
+  }
+  table.className = currentClassName
+}
