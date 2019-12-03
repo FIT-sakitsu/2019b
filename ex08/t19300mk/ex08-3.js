aadcf8180cef7d9c4728ba0a0f4ff12fabd3fe33
Marie Koura <t19300mk@zmac158.sfc.keio.ac.jp>
2019-12-03 11:34:34 +0900

    ex08

---
+++ ex08/t19300mk/ex08-3.js
@@ -0,0 +1,45 @@
+function na3() {
+  var str = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ]
+  var ele = document.getElementById('text080202')
+
+  var t_start = new Date().getTime()
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
+      for (var k = 0; k < str.length; k++) {
+        ele.value = str[i] + str[j] + str[k]
+      }
+    }
+  }
+
+  var t_end = new Date().getTime()
+
+  alert('かかった時間は' + (t_end - t_start) + 'ミリ秒です')
+}
---
+++ ex08/t19300mk/ex08-5.js
@@ -0,0 +1,24 @@
+function na3() {
+  var str = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  var ele = document.getElementById('text080202')
+
+  var t_start = new Date().getTime()
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
+      for (var k = 0; k < str.length; k++) {
+        for (var l = 0; l < str.length; l++) {
+          for (var m = 0; m < str.length; m++) {
+            for (var n = 0; n < str.length; n++) {
+              ele.value = str[i] + str[j] + str[k] + str[l] + str[m] + str[n]
+            }
+          }
+        }
+      }
+    }
+  }
+
+  var t_end = new Date().getTime()
+  var ele2 = document.getElementById('test')
+  ele2.value = t_end - t_start
+}
