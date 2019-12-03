64b17fa98ae77da7b69836942def54afb0bcb785
Yumiko Toyofuku <t19534yt@zmac135.sfc.keio.ac.jp>
2019-12-03 12:19:52 +0900

    ex07-6を修正

--- ex07/t19534yt/ex07-6.js
+++ ex07/t19534yt/ex07-6.js
@@ -2,10 +2,21 @@ var canvas = document.getElementById('canvas')
 
 var ctx = canvas.getContext('2d')
 
+ctx.fillStyle = 'lightgray'
 var x, y
 for (y = 0; y < 200; y += 20) {
   for (x = 0; x < 200; x += 20) {
     if ((x + y) % 40 == 0) {
+      ctx.fillRect(x, y, 20, 20)
+    }
+  }
+}
+
+ctx.fillStyle = 'black'
+var x, y
+for (y = 0; y < 200; y += 20) {
+  for (x = 0; x < 200; x += 20) {
+    if ((x + y) % 80 == 0) {
       ctx.fillRect(x, y, 20, 20)
     }
   }
