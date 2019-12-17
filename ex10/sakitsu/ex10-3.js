02a5db2116e6d07920754525fa332e9ce793a1ff
Itsuki Sakitsu <itsuki.sakitsu@gmail.com>
2019-12-17 10:26:55 +0900

    ex10-3をといた

---
+++ ex10/sakitsu/ex10-3.js
@@ -0,0 +1,41 @@
+// ゲームスタート
+var score = 0
+
+$(function() {
+  $('button').click(function() {
+    start()
+  })
+})
+
+// ゲーム開始
+function start() {
+  score = 0
+  setTimeout(end, 10000)
+  reDraw()
+}
+
+// ゲーム終了
+function end() {
+  alert(score)
+}
+
+// モグラを再描画
+function reDraw() {
+  $('td')
+    .html('○') // tdタグの中身をすべて○に書き換え
+    .off() // eventをすべて除去
+
+  // 全tdタグのなかからランダムな1個を選択
+  var randomCell = Math.floor(Math.random() * $('td').length)
+  $('td')
+    .eq(randomCell) // ランダムな要素だけを抽出
+    .html('◎') // モグラマークに修正
+    .click(addScore) // クリックしたら点数が増えるように
+    .click(reDraw) // クリックしたらモグラを再描画
+}
+
+// 点数を追加
+function addScore() {
+  score++
+  $('#score').html(score)
+}
