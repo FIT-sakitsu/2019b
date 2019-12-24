c3936c1e48c07502631fe81196dbacc653af6cef
Hinata Maeda <s19723hm@zmac155.sfc.keio.ac.jp>
2019-12-24 11:40:03 +0900

    ex11を作成

---
+++ ex11/s19723hm/ex11-2.js
@@ -0,0 +1,26 @@
+var map
+
+function clickCallback(event) {
+  alert(event.latlng)
+}
+
+$(function() {
+  map = L.map('map')
+
+  map.setView([35.388276, 139.427348], 15)
+
+  map.on('click', function(e) {
+    lat = e.latlng.lat
+    lng = e.latlng.lng
+
+    alert('lat: ' + lat + ', lng: ' + lng)
+  })
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>' +
+      'contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  }).addTo(map)
+
+  map.on('click', clickCallback)
+})
