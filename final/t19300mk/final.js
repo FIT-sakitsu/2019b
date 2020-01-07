ee72b3f8687c4a6ccaf9160d6027635a0b07c4c9
mariekoura <t19300mk@sfc.keio.ac.jp>
2020-01-07 12:36:06 +0900

    追加

---
+++ final/t19300mk/final.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('h1').fadeOut(3000);
+  $(function() {
+    $('#accordion dd').hide();
+    $('#accordion dt a').click(function(){
+         $('#accordion dd').slideUp();
+         $(this).parent().next().slideDown();
+         return false;
+     });
+});
+  $('.scroll_exe').click(function(){
+    $('html,body').animate({scrollTop:0})
+  })
+})
