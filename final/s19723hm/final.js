58fcd257d7916b8595e107e60377c9f348ab280d
前田陽汰 <maedahinata@maedaakiranoMacBook-Air.local>
2020-01-18 10:41:55 +0900

    提出します。

--- final/s19723hm/final.js
+++ final/s19723hm/final.js
@@ -1,18 +1,38 @@
 // "map" divに地図を作成し、指定した場所とズームで表示する
-var map = L.map('map').setView([35.3, 139.4], 6);
+var map = L.map('map')
+map.setView([35.3, 139.4], 6)
 
 // OpenStreetMapタイルレイヤーを追加する
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-}).addTo(map);
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+}).addTo(map)
 
-const textbox = document.getElementById("textbox").value;
-const exe = document.getElementById("exe");
-exe.onclick = ()=>{
-  if(textbox === "kantou"){
-    map = L.map('map').setView([35.3, 139.4], 10);
+const exe = document.getElementById('exe')
+exe.onclick = () => {
+  var textbox = document.getElementById('textbox').value
+  if (textbox === 'kanto') {
+    map.setView([35.3, 139.4], 9)
+  } else if (textbox === 'kansai') {
+    map.setView([34.4138, 135.3112], 9)
+  } else if (textbox === 'kyushu') {
+    map.setView([33.3623, 130.2505], 9)
+  } else if (textbox === 'tohoku') {
+    map.setView([39.4207, 141.0915], 9)
   }
+
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-  }).addTo(map);
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  }).addTo(map)
+}
+
+var lists = ['kanto', 'kansai', 'kyushu', 'tohoku']
+for (var i = 0; i < lists.length; i++) {
+  console.log(lists[i])
+}
+
+var elem = document.getElementById('areas')
+for (var i = 0; i < lists.length; i++) {
+  elem.innerHTML += '<p>' + lists[i] + '</p>'
 }
