3be0ce8c0bf1c1c6ccc763cddc51af6add3eaa41
前田陽汰 <maedahinata@maedaakiranoMacBook-Air.local>
2020-01-18 09:24:47 +0900

    進捗です

--- final/s19723hm/final.js
+++ final/s19723hm/final.js
@@ -1,1 +1,18 @@
-alert()
+// "map" divに地図を作成し、指定した場所とズームで表示する
+var map = L.map('map').setView([35.3, 139.4], 6);
+
+// OpenStreetMapタイルレイヤーを追加する
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+}).addTo(map);
+
+const textbox = document.getElementById("textbox").value;
+const exe = document.getElementById("exe");
+exe.onclick = ()=>{
+  if(textbox === "kantou"){
+    map = L.map('map').setView([35.3, 139.4], 10);
+  }
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+  }).addTo(map);
+}
