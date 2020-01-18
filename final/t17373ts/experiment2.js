eb82d1a0919f16b42db70b1553700b389b02fb13
Takumi Shimada <t17373ts@zmac122.sfc.keio.ac.jp>
2020-01-18 13:16:51 +0900

    1/18 編集分

--- final/t17373ts/experiment2.js
+++ final/t17373ts/experiment2.js
@@ -1,27 +1,32 @@
 var Reversi = function() {
-  var canvas = document.getElementById('canvas')
-  var ctx = canvas.getContext('2d') //canvas作製
+  // canvas
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d') //canvas作製
 
-  var column = 8 //盤面横マス数
-  var row = 8 //盤面縦マス数
-  var cellWidth = 500 / column
-  var stoneRadius = (cellWidth * 0.8) / 2 //コマの直径
+  // settings
+  const columnSize = 8 //盤面横マス数
+  const rowSize = 8 //盤面縦マス数
+  const cellWidth = 500 / columnSize
+  const stoneRadius = (cellWidth * 0.8) / 2 //コマの直径
+  const black = 0
+  const white = 1
+  const empty = 2
+  const maskX = [-1, 0, 1, -1, 1, -1, 0, 1]
+  const maskY = [-1, -1, -1, 0, 0, 1, 1, 1]
 
+  // params
   var board = []
+  var passedLastTurn = false
+  var player = black
+  var counterPlayer = white
 
-  //ルーチン関数s
-  var twoDToOneD = function(x, y) {
-    return y * row + x
+  // 補助関数
+  var twoDToOneD = function(col, row) {
+    return row * rowSize + col
   }
+
   var oneDToTwoD = function(idx) {
-    return [idx % row, Math.floor(idx / row)]
-  }
-  var xyInRange = function(x, y) {
-    if (x < 0 || x >= row) return false //左端と右端が
-    if (y < 0 || y >= column) return false //上端と下端が
-    if (maskX[i] < 0 || maskX[i] >= row) return false //左端と右端が
-    if (maskY[i] < 0 || maskY[i] >= column) return false //上端と下端が
-    return true
+    return [idx % rowSize, Math.floor(idx / rowSize)]
   }
 
   var coordsToIdx = function(x, y) {
@@ -30,50 +35,62 @@ var Reversi = function() {
     var i = Math.floor(y / cellWidth)
     return twoDToOneD(j, i) //
   }
-  //end ルーチン関数s
+
+  var xyInRange = function(x, y) {
+    if (x < 0 || x >= rowSize) return false //左端と右端が
+    if (y < 0 || y >= columnSize) return false //上端と下端が
+    if (maskX[i] < 0 || maskX[i] >= rowSize) return false //左端と右端が
+    if (maskY[i] < 0 || maskY[i] >= columnSize) return false //上端と下端が
+    return true
+  }
+
+  var isEmptyCell = function(idx) {
+    return board[idx] == empty
+  }
+  //end 補助関数
 
   var render = function() {
-    //render 描画する関数
+    // 盤面
     ctx.fillStyle = '#080'
-    ctx.fillRect(0, 0, cellWidth * row, cellWidth * column)
-    for (i = 0; i < column + 1; i++) {
+    ctx.fillRect(0, 0, cellWidth * rowSize, cellWidth * columnSize)
+
+    // 列の罫線
+    for (i = 0; i < columnSize + 1; i++) {
       var y = i * cellWidth
       ctx.beginPath()
       ctx.moveTo(0, y)
-      ctx.lineTo(cellWidth * row, y)
-      ctx.closePath()
-      ctx.strokeStyle = '#000'
-      ctx.stroke()
-    }
-    for (i = 0; i < row + 1; i++) {
-      var x = i * cellWidth
-      ctx.beginPath()
-      ctx.moveTo(x, 0)
-      ctx.lineTo(x, cellWidth * column)
+      ctx.lineTo(cellWidth * rowSize, y)
       ctx.closePath()
       ctx.strokeStyle = '#000'
       ctx.stroke()
     }
 
-    for (i = 0; i < column; i++) {
+    // 行の罫線
+    for (i = 0; i < rowSize + 1; i++) {
+      var x = i * cellWidth
+      ctx.beginPath()
+      ctx.moveTo(x, 0)
+      ctx.lineTo(x, cellWidth * columnSize)
+      ctx.closePath()
+      ctx.strokeStyle = '#000'
+      ctx.stroke()
+    }
+
+    // コマを描画
+    for (i = 0; i < columnSize; i++) {
       var y = i * cellWidth
-      for (j = 0; j < row; j++) {
+      for (j = 0; j < rowSize; j++) {
         var x = j * cellWidth
-        //
-        /*if ((i + j) % 2 === 0) {
-          //チェッカーボード
-          ctx.fillStyle = '#000'
-          ctx.fillRect(x, y, cellWidth, cellWidth)
-        }*/
+
         //オセロ本体
         var idx = twoDToOneD(j, i) //検索する基準のマス目
         var stoneColor = board[idx]
-        if (stoneColor !== 2) {
+        if (!isEmptyCell(idx)) {
           ctx.beginPath()
           ctx.arc(x + cellWidth / 2, y + cellWidth / 2, stoneRadius, 0, 6.28)
           ctx.closePath()
           ctx.strokeStyle = '#fff'
-          if (stoneColor === 0) {
+          if (stoneColor === black) {
             ctx.fillStyle = '#000'
           } else {
             ctx.fillStyle = '#fff'
@@ -87,19 +104,15 @@ var Reversi = function() {
 
   var init = function() {
     //ゲームが終わった時・ゲームを最初に描画する時に最初の盤面を描画するfunction
-    for (var i = 0; i < column * row; i++) {
-      /*
-      0 is black
-      1 is white
-      2 is empty
-      */
-      board[i] = 2 //2 is empty
+    for (var i = 0; i < columnSize * rowSize; i++) {
+      board[i] = empty //2 is empty
     }
     //fill the center 4
-    board[27] = 0
-    board[28] = 1
-    board[35] = 1
-    board[36] = 0
+
+    board[27] = black
+    board[28] = white
+    board[35] = white
+    board[36] = black
 
     turn.innerHTML = '黒のターン！黒側が操作してください。'
 
@@ -107,15 +120,8 @@ var Reversi = function() {
     document.getElementsByName('whiteCount')[0].value = 2
     render()
   }
-  init()
 
   this.init = init
-  this.render = render
-  color = 0
-  antiColor = 1
-
-  var maskX = [-1, 0, 1, -1, 1, -1, 0, 1]
-  var maskY = [-1, -1, -1, 0, 0, 1, 1, 1]
 
   var findFlipped = function(idx) {
     var xy = oneDToTwoD(idx) //xyが２次元配列、idxが1次元配列
@@ -124,17 +130,17 @@ var Reversi = function() {
       if (xyInRange(xy[0] + maskX[i], xy[1] + maskY[i])) {
         //if in range
         var idx2 = twoDToOneD(xy[0] + maskX[i], xy[1] + maskY[i]) //idxの周りの8マス座標の配列
-        if (board[idx2] === antiColor) {
+        if (board[idx2] === counterPlayer) {
           //different color: begin search
-          //console.log(idx2,board[idx2],antiColor);
+          //console.log(idx2,board[idx2],counterPlayer);
           var j = 2
           var matches = [idx2]
           while (true) {
             if (xyInRange(xy[0] + maskX[i] * j, xy[1] + maskY[i] * j)) {
               var idx3 = twoDToOneD(xy[0] + maskX[i] * j, xy[1] + maskY[i] * j) //周りに8マス以降放射状に伸びた先のマス
-              if (board[idx3] === antiColor) {
+              if (board[idx3] === counterPlayer) {
                 matches.push(idx3)
-              } else if (board[idx3] === color) {
+              } else if (board[idx3] === player) {
                 //found match, add pairs
                 flipped.push(matches)
                 break
@@ -152,10 +158,10 @@ var Reversi = function() {
     return flipped
   }
 
-  var changeColor = function() {
-    var c0 = color
-    color = antiColor
-    antiColor = c0
+  var changePlayer = function() {
+    var tmp = player
+    player = counterPlayer
+    counterPlayer = tmp
   }
 
   var countStones = function() {
@@ -164,10 +170,10 @@ var Reversi = function() {
 
     for (var i = 0; i < board.length; i++) {
       switch (board[i]) {
-        case 0:
+        case black:
           blackCount++
           break
-        case 1:
+        case white:
           whiteCount++
           break
       }
@@ -181,22 +187,22 @@ var Reversi = function() {
     var x = e.offsetX
     var y = e.offsetY
     var idx = coordsToIdx(x, y)
-    if (board[idx] === 2) {
+    if (isEmptyCell(idx)) {
       var flipped = findFlipped(idx)
       if (flipped.length === 0) {
         console.log("you can't place there")
         turn.innerHTML = 'そこには置けません！もう一度操作してください。'
         return false
       } else {
-        board[idx] = color
+        board[idx] = player
         for (var i = 0; i < flipped.length; i++) {
           for (var j = 0; j < flipped[i].length; j++) {
-            board[flipped[i][j]] = color
+            board[flipped[i][j]] = player
           }
         }
         render()
-        changeColor()
-        if (color === 0) {
+        changePlayer()
+        if (player === 0) {
           console.log("Black's turn")
           turn.innerHTML = '黒のターン！黒側が操作してください。'
         } else {
@@ -204,22 +210,40 @@ var Reversi = function() {
           turn.innerHTML = '白のターン！白側が操作してください。'
         }
       }
+      countStones()
+
+      if (!canPutStone()) {
+        if (passedLastTurn) {
+          alert('ゲーム終了')
+          console.log('Game Over')
+          window.location.href = 'finalResultScreen.html'
+        }
+
+        // 次のプレイヤーはぱすせざるを得ない
+        alert('置ける場所がありません！相手にターンを回します！')
+        changePlayer()
+        passedLastTurn = true
+      } else {
+        passedLastTurn = false
+      }
     }
-    countStones()
-    gameSet()
   }
 
   canvas.addEventListener('click', canvasOnclick.bind(this), false)
 
-  var gameSet = function() {
-    if (
-      parseInt(document.getElementById('blackCountHtml').value, 10) +
-        parseInt(document.getElementById('whiteCountHtml').value, 10) ===
-      64
-    )
-      alert('ゲーム終了！')
-    window.location.href = finalResultScreen.html
+  var canPutStone = function() {
+    var canPlace = []
+    for (var idx = 0; idx < board.length; idx++) {
+      if (isEmptyCell(idx)) {
+        var flipped = findFlipped(idx)
+        if (0 < flipped.length) {
+          canPlace.push(idx)
+        }
+      }
+    }
+    return canPlace.length > 0
   }
 }
 
 var reversi = new Reversi()
+reversi.init()
